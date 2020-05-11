0c8788eb13dd3c661d88f8688374b0c8966e4d3e
Shikhar Sharma <ceoshikhar@gmail.com>
2020-05-12 01:10:44 +0530

    update

--- my_express_server/server.js
+++ my_express_server/server.js
@@ -1,8 +1,12 @@
-var express = require("express");
-var app =  express()
+const express = require('express');
 
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>This shit works yo</h1>');
+});
 
 app.listen(3000, function() {
-	console.log("Server has started");
-})
+  console.log("Server has started at http://localhost:3000");
+});
 
