80954b296d2dac0e72e21facc8440d798819a5e8
Shikhar Sharma <ceoshikhar@gmail.com>
2020-05-15 16:00:40 +0530

    Calculator App

---
+++ calculator/calculator.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+// Parse the data
+app.use(bodyParser.urlencoded({ extended: true  }));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Do the calculation
+app.post('/', (req, res) => {
+  var n1 = Number(req.body.num1);
+  var n2 = Number(req.body.num2);
+  var result = n1 + n2;
+  
+  res.send('The result is: ' + result);
+});
+
+// Start the server 
+app.listen(3000, console.log('Server started at http://localhost:3000'));
